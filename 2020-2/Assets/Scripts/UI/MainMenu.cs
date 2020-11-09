0b839872b03da45f9e6ef9c32e39f04f1792a27e
juan camilo oscategui rodriguez <oscateguic@gmail.com>
2020-11-08 20:20:44 -0500

    Main Menu

---
+++ 2020-2/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+ {
+     public Animator anim;
+    public void playGame(){
+        //Activar animacion
+        anim.SetTrigger("Start");
+
+        //Invocar corrutina 
+        StartCoroutine(LoadLevel(1));
+    }
+
+    public void quitGame(){
+        Application.Quit();
+    }
+
+    public IEnumerator LoadLevel(int buildIndex){
+
+        //Esperar y cambiar
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(buildIndex);
+    }
+}
